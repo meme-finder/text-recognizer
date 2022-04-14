b49b0f56be418aadf9a00aad46eadd0cb2a98348
Alexander Averyanov <alex@averyan.ru>
2022-04-15 00:06:01 +0300

    Tesseract OCR

--- recognizer/main.py
+++ recognizer/main.py
@@ -1,13 +1,16 @@
-from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.encoders import jsonable_encoder
-import uuid
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+import pytesseract
+import io
 
 
 app = FastAPI()
+print(pytesseract.get_languages(config=''))
 
 
 @app.post("/")
-async def recognize_image() -> int:
-    return 0
+async def recognize_image(file: UploadFile) -> str:
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+    text = pytesseract.image_to_string(image, lang='osd')
+    return text
